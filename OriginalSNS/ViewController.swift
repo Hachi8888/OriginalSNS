f8e959c561010bfecaeed619973be71c6f1a81b5
Hachi8888 <h.nana.420@gmail.com>
2019-08-20 16:52:57 +0900

    HOWラベルと表示の機能追加

--- OriginalSNS/ViewController.swift
+++ OriginalSNS/ViewController.swift
@@ -10,71 +10,83 @@ import UIKit
 
 
 class ViewController: UIViewController {
-    // 名詞を表示させるラベル
-    @IBOutlet weak var subjectLabel: UILabel!
-    // 動詞を表示させるラベル
-    @IBOutlet weak var verbLabel: UILabel!
 
-    // 各ラベル、ボタンの表示を切り替えるために使用する変数3つ
+    // WHAT: 対象を表示させるラベル
+    @IBOutlet weak var whatLabel: UILabel!
+    // TO DO?: 動詞を表示させるラベル
+    @IBOutlet weak var toDoLabel: UILabel!
+    // HOW: 形容詞を表示させるラベル
+    @IBOutlet weak var howLabel: UILabel!
+
+    // ボタン、各ラベルの表示を切り替えるために使用する変数4つ
     var tapCount: Int = 0
-    var subjectCount: Int = 0
-    var verbCount: Int = 0
+    var whatCount: Int = 0
+    var toDoCount: Int = 0
+    var howCount: Int = 0
 
-    // 名詞ラベルに表示させる語句一覧(語句追加するので、varで)
-    var subjectList: [String] = ["青色のものを", "黄色のものを", "緑色のものを", "赤色のものを", "つやつやしたものを", "ざらざらしたものを", "トゲトゲしたものを", "やわらかいものを", "硬いものを", "長いものを", "辛いものを", "甘いものを", "漢字で書いたときに画数の多いものを"]
+    // 各ラベルに表示させる語句の配列一覧(語句追加するのでvarで宣言)
+    // 対象リスト
+    var whatList: [String] = ["青色のものを", "黄色のものを", "緑色のものを", "赤色のものを", "つやつやしたものを", "ざらざらしたものを", "トゲトゲしたものを", "やわらかいものを", "硬いものを", "長いものを", "辛いものを", "甘いものを", "漢字で書いたときに画数の多いものを", "細いものを", "高さがあるものを"]
 
-    // 動詞ラベルに表示させる語句一覧(語句追加するので、varで)
-    var verbList: [String] = ["食べる", "触る", "買う", "さりげなく人と話す", "写真に取る", "街で見つける", "辞書で意味を調べる", "綺麗な字で書く"]
+    // 動詞リスト
+    var toDoList: [String] = ["食べる", "触る", "買う", "人と話す", "写真に取る", "街で見つける", "辞書で意味を調べる", "綺麗な字で書く"]
+
+    // 【追加機能】副詞リスト
+    var howList: [String] = ["すばやく", "ゆっくりと", "笑顔で", "元気よく", "さりげなく", "心をこめて", "無表情で", "おもむろに", "どざくさにまぎれて", "目をつむって", "おおげさに"]
 
     override func viewDidLoad() {
         super.viewDidLoad()
 
-        // 各ラベルのデフォルトの表示を設定
-        subjectLabel.text = "WHAT"
-        verbLabel.text = "TO DO?"
-
         }
 
-
-    // START,STOPボタン
-    /// デフォルト値は START で、押すたびに STOP と START の表示が入れ替わる
-     // FIXME: 動詞、名詞ごとにボタンをつくること、関数にまとめること
-
+    // 【メイン機能】ボタンを押して、WHAT TODO? HOW の3要素を決定
+    // FIXME: 関数にまとめる箇所がないかチェック!
     @IBAction func tappedButton(_ sender: UIButton) {
 
        // ボタンを押したときのカウントを進める
        tapCount += 1
        print(tapCount)
 
-       // 対象に表示させるものを配列からランダムで選択する
-       subjectCount = Int.random(in: 0..<subjectList.count)
+       // WHAT に表示させるものを対象の配列からランダムで決定
+       whatCount = Int.random(in: 0..<whatList.count)
+
+       // ボタンの表示を START → STOP へ変更
        sender.setTitle("STOP", for: .normal)
 
         switch tapCount {
         case 1:
-            // 名詞を決定
-            // FIXME: 関数にまとめられる?
+            // 3つのラベルを??に変更
             // FIXME: ?? は gifで表現できると楽しいかも
-            subjectLabel.text = "??"
-            verbLabel.text = "??"
+            whatLabel.text = "??"
+            toDoLabel.text = "??"
+            howLabel.text = "??"
 
         case 2:
-            subjectLabel.text = subjectList[subjectCount]
+            // WHAT を表示
+            whatLabel.text = whatList[whatCount]
+            // ボタンの表示を STOP → START へ変更
             sender.setTitle("START", for: .normal)
-
-            verbCount = Int.random(in: 0..<verbList.count)
+            // TO DO? に表示させるものを対象の配列からランダムで決定
+            toDoCount = Int.random(in: 0..<toDoList.count)
 
             // FIXME: 休憩をはさんだほうが良い?
 
         case 3:
-            verbLabel.text = verbList[verbCount]
+            // TO DO? を表示する
+            toDoLabel.text = toDoList[toDoCount]
+            // ボタンの表示を START → STOP へ変更
             sender.setTitle("STOP", for: .normal)
-
-            // FIXME: 動詞ラベルにgifを追加してね
+            // HOW に表示させるものを対象の配列からランダムで決定
+            howCount = Int.random(in: 0..<howList.count)
 
         case 4:
-            // FIXME: 1週間ボタンを押せないようにする、画面に決定したお題を表示する、画面をだしてくる?
+            // HOW を表示
+            howLabel.text = howList[howCount]
 
+            // FIXME: 画面に決定したお題を表示する、画面をだしてくる?
+
+            // FIXME: 一定期間(例:24時間)はお題を出せないようにする?
+            // 表示をもとに戻す
             tapCount = 0
 
         default:
