9ac2b3fa25de77a4944276342b09d3c8e929896c
Hachi8888 <h.nana.420@gmail.com>
2019-08-22 18:42:46 +0900

    ログイン画面のレイアウト修正

--- OriginalSNS/View/Test/TestViewController.swift
+++ OriginalSNS/View/Test/TestViewController.swift
@@ -7,24 +7,69 @@
 //
 
 import UIKit
+import BubbleTransition
 
-class TestViewController: UIViewController {
+class TestViewController: UIViewController , UIViewControllerTransitioningDelegate{
+
+    @IBOutlet weak var nextButton: UIButton!
+
+    let transition = BubbleTransition()
 
     override func viewDidLoad() {
         super.viewDidLoad()
 
         // Do any additional setup after loading the view.
     }
-    
 
-    /*
-    // MARK: - Navigation
 
-    // In a storyboard-based application, you will often want to do a little preparation before navigation
-    override func prepare(for segue: UIStoryboardSegue, sender: Any?) {
-        // Get the new view controller using segue.destination.
-        // Pass the selected object to the new view controller.
+    public override func prepare(for segue: UIStoryboardSegue, sender: Any?) {
+        let controller = segue.destination
+        controller.transitioningDelegate = self
+        controller.modalPresentationStyle = .custom
     }
-    */
+
+
+    public func animationController(forPresented presented: UIViewController, presenting: UIViewController, source: UIViewController) -> UIViewControllerAnimatedTransitioning? {
+        transition.transitionMode = .present
+        transition.startingPoint = nextButton.center
+        transition.bubbleColor = nextButton.backgroundColor!
+        return transition
+    }
+
+    public func animationController(forDismissed dismissed: UIViewController) -> UIViewControllerAnimatedTransitioning? {
+        transition.transitionMode = .dismiss
+        transition.startingPoint = nextButton.center
+        transition.bubbleColor = nextButton.backgroundColor!
+        return transition
+    }
+
+
+}
+
+//  ★別のクラスを作ります!!★
+
+class Test2ViewController: UIViewController {
+
+    // dismiss用
+    var interactiveTransition = BubbleInteractiveTransition()
+
+override func prepare(for segue: UIStoryboardSegue, sender: Any?) {
+    if let controller = segue.destination as? Test2ViewController {
+        controller.transitioningDelegate = self as? UIViewControllerTransitioningDelegate
+        controller.modalPresentationStyle = .custom
+        controller.interactiveTransition = interactiveTransition
+        interactiveTransition.attach(to: controller)
+    }
+}
+
+// dismiss用
+func interactionControllerForDismissal(using animator: UIViewControllerAnimatedTransitioning) -> UIViewControllerInteractiveTransitioning? {
+    return interactiveTransition
+}
+
+    @IBAction func backButton(_ sender: Any) {
+        self.dismiss(animated: true, completion: nil)
+
+    }
 
 }
