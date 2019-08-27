a323b1f10fa64daea172c53ed250a3e89971574b
Hachi8888 <h.nana.420@gmail.com>
2019-08-27 21:27:13 +0900

    goodListVCに表示する投稿の各要素をgoodListTVCに紐付け完了

---
+++ OriginalSNS/View/TimeLine/GoodListTableViewCell.swift
@@ -0,0 +1,35 @@
+//
+//  GoodListTableViewCell.swift
+//  OriginalSNS
+//
+//  Created by VERTEX22 on 2019/08/27.
+//  Copyright © 2019 N-project. All rights reserved.
+//
+
+import UIKit
+import IBAnimatable
+
+class GoodListTableViewCell: UITableViewCell {
+
+    // いいねされた投稿を表すための書く要素の紐付け
+    // プロフィール画像
+    @IBOutlet weak var gooListIconImageView: AnimatableImageView!
+    // 名前ラベル
+    @IBOutlet weak var goodListNameLabel: UILabel!
+    // 投稿画像
+    @IBOutlet weak var goodListPostImageView: AnimatableImageView!
+    // 投稿文
+    @IBOutlet weak var goodListTextView: AnimatableTextView!
+
+    override func awakeFromNib() {
+        super.awakeFromNib()
+        // Initialization code
+    }
+
+    override func setSelected(_ selected: Bool, animated: Bool) {
+        super.setSelected(selected, animated: animated)
+
+        // Configure the view for the selected state
+    }
+
+}
---
+++ OriginalSNS/View/TimeLine/GoodListViewController.swift
@@ -0,0 +1,30 @@
+//
+//  GoodListViewController.swift
+//  OriginalSNS
+//
+//  Created by VERTEX22 on 2019/08/27.
+//  Copyright © 2019 N-project. All rights reserved.
+//
+
+import UIKit
+
+class GoodListViewController: UIViewController {
+
+    override func viewDidLoad() {
+        super.viewDidLoad()
+
+        // Do any additional setup after loading the view.
+    }
+    
+
+    /*
+    // MARK: - Navigation
+
+    // In a storyboard-based application, you will often want to do a little preparation before navigation
+    override func prepare(for segue: UIStoryboardSegue, sender: Any?) {
+        // Get the new view controller using segue.destination.
+        // Pass the selected object to the new view controller.
+    }
+    */
+
+}
